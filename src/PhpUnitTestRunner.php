5b24231216bb3f3c376382cca8be5108ef73d743
Luis Rovirosa <luisrovirosa@gmail.com>
2017-07-12 19:00:04 +0200

    PhpUnitTestRunner the_tests_pass_when_there_are_tests_to_execute_with_successful_result

--- src/PhpUnitTestRunner.php
+++ src/PhpUnitTestRunner.php
@@ -6,6 +6,26 @@ class PhpUnitTestRunner implements TestRunner
 {
     public function runTests(Tests $tests): ExecutionResult
     {
-        return new ExecutionResult(true, 0);
+        $hasPassed = true;
+        $numberOfTests = 0;
+        /** @var Test $test */
+        foreach ($tests->getTests() as $test){
+            $command = "./vendor/bin/phpunit --filter '" . $this->classNameForTest($test) . "'";
+            exec($command, $output);
+            $lastOutput = $output[count($output) - 1];
+            $hasPassed = strpos($lastOutput, 'OK') !== false;
+            preg_match('/.*\(([^\ ]*)\ .*/',$lastOutput, $matches);
+            $numberOfTests = $matches[1];
+        }
+        return new ExecutionResult($hasPassed, $numberOfTests);
+    }
+
+    /**
+     * @param Test $test
+     * @return mixed
+     */
+    private function classNameForTest(Test $test)
+    {
+        return str_replace('\\', '\\\\', $test->fullyQualifyClassName());
     }
 }
--- tests/unit/PhpUnitTestRunnerTest.php
+++ tests/unit/PhpUnitTestRunnerTest.php
@@ -3,6 +3,7 @@
 namespace JlDojo\SonicTest\Tests\Unit;
 
 use JlDojo\SonicTest\PhpUnitTestRunner;
+use JlDojo\SonicTest\Test;
 use JlDojo\SonicTest\Tests;
 use PHPUnit\Framework\TestCase;
 
@@ -17,5 +18,17 @@ class PhpUnitTestRunnerTest extends TestCase
 
         $this->assertTrue($executionResult->hasPassed());
         $this->assertEquals(0, $executionResult->numberOfTests());
+    }
+
+    /** @test */
+    public function the_tests_pass_when_there_are_tests_to_execute_with_successful_result()
+    {
+        $runner = new PhpUnitTestRunner();
+
+        $test = new Test('JlDojo\SonicTest\Tests\ProjectToTest\Tests\SimpleProductionCodeTest');
+        $executionResult = $runner->runTests(new Tests([$test]));
+
+        $this->assertTrue($executionResult->hasPassed());
+        $this->assertEquals(1, $executionResult->numberOfTests());
     }
 }
