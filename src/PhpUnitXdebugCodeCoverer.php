f3c2fa0d2c9d0874f8b6448a3707f3479e66063a
Luis Rovirosa <luisrovirosa@gmail.com>
2017-07-12 13:45:28 +0200

    CodeCoverer the_test_that_covers_a_file_is_returned

--- src/PhpUnitXdebugCodeCoverer.php
+++ src/PhpUnitXdebugCodeCoverer.php
@@ -4,8 +4,50 @@ namespace JlDojo\SonicTest;
 
 class PhpUnitXdebugCodeCoverer implements CodeCoverer
 {
+    const PREFIX = '/tests/data';
+
     public function cover(): CodeCoverage
     {
-        return new CodeCoverage([]);
+        exec('./vendor/bin/phpunit -c ' . self::PREFIX . ' --whitelist ' . self::PREFIX . '/src  --coverage-php file.php');
+        /** @var \SebastianBergmann\CodeCoverage\CodeCoverage $coverage */
+        $coverage = include './file.php';
+        $rawCodeCoverage = $coverage->getData();
+        $coveredFiles = $this->removeWorkingDirectoryFromFilePath($rawCodeCoverage);
+        return new CodeCoverage($this->calculateTestToExecute($coveredFiles));
+    }
+
+    /**
+     * @param $rawCodeCoverage
+     * @return array
+     */
+    private function removeWorkingDirectoryFromFilePath($rawCodeCoverage): array
+    {
+        $keys = array_map(function ($path) {
+            return str_replace(dirname(__DIR__) . self::PREFIX, '.', $path);
+        }, array_keys($rawCodeCoverage));
+        return array_combine($keys, $rawCodeCoverage);
+    }
+
+    /**
+     * @param $cleanedKeys
+     * @return array
+     */
+    private function calculateTestToExecute($cleanedKeys): array
+    {
+        return array_map(function ($listOfTests) {
+            if (count($listOfTests) == 0) {
+                return $listOfTests;
+            }
+            $testNotNull = array_filter($listOfTests, function ($elements) {
+                return is_array($elements);
+            });
+
+            $elements = [];
+            foreach ($testNotNull as $tests) {
+                $elements = array_merge($elements, $tests);
+            }
+
+            return $elements;
+        }, $cleanedKeys);
     }
 }
--- tests/unit/PhpUnitXdebugCodeCovererTest.php
+++ tests/unit/PhpUnitXdebugCodeCovererTest.php
@@ -7,7 +7,8 @@ use PHPUnit\Framework\TestCase;
 
 class PhpUnitXdebugCodeCovererTest extends TestCase
 {
-    const UNCOVERED_PRODUCTION_CODE = 'src/UncoveredProductionCode.php';
+    const UNCOVERED_PRODUCTION_CODE = './src/UncoveredProductionCode.php';
+    const SIMPLE_PRODUCTION_CODE    = './src/SimpleProductionCode.php';
 
     /** @test */
     public function there_is_no_test_related_to_an_uncovered_file()
@@ -17,5 +18,15 @@ class PhpUnitXdebugCodeCovererTest extends TestCase
         $codeCoverage = $codeCoverer->cover();
 
         $this->assertEquals([], $codeCoverage->testsRelatedTo(self::UNCOVERED_PRODUCTION_CODE));
+    }
+
+    /** @test */
+    public function the_test_that_covers_a_file_is_returned()
+    {
+        $codeCoverer = new PhpUnitXdebugCodeCoverer();
+
+        $codeCoverage = $codeCoverer->cover();
+
+        $this->assertCount(1, $codeCoverage->testsRelatedTo(self::SIMPLE_PRODUCTION_CODE));
     }
 }
