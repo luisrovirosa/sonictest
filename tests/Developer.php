8bc043bcf8a65ce7aa11e47d3e70398e4dc53779
Luis Rovirosa <luisrovirosa@gmail.com>
2017-07-12 19:52:25 +0200

    Developer changed to the new production code

--- tests/Developer.php
+++ tests/Developer.php
@@ -9,14 +9,14 @@ class Developer
     public function changeSimpleProductionCode()
     {
         $content = file_get_contents(self::SIMPLE_PRODUCTION_CODE_PATH);
-        $changedContent = str_replace('return true;', "echo 'hello';\nreturn true;", $content);
+        $changedContent = str_replace('$result = true;', "echo 'hello';\n\$result = true;", $content);
         file_put_contents(self::SIMPLE_PRODUCTION_CODE_PATH, $changedContent);
     }
 
     public function revertProductionCode()
     {
         $content = file_get_contents(self::SIMPLE_PRODUCTION_CODE_PATH);
-        $changedContent = str_replace("echo 'hello';\nreturn true;", 'return true;', $content);
+        $changedContent = str_replace("echo 'hello';\n\$result = true;", '$result = true;', $content);
         file_put_contents(self::SIMPLE_PRODUCTION_CODE_PATH, $changedContent);
     }
 
